ad45a0ff8b42061b6a4df8eb3f3e32f411c4b4fc
beatrizespinoza123 <beatrizespinoza0819@gmail.com>
2019-01-30 10:52:37 -0600

    inicio de proyecto

---
+++ src/prueba2git/Prueba2git.java
@@ -0,0 +1,34 @@
+
+package prueba2git;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Prueba2git {
+
+   public static void main (String [] args)
+   {
+      InputStreamReader isr = new InputStreamReader(System.in);
+      BufferedReader br = new BufferedReader (isr);
+      try
+      {
+         System.out.print("Sumando 1 : ");
+         System.out.printf("hola bety");
+         int s1 = Integer.parseInt(br.readLine());
+         System.out.print("Sumando 2 : ");
+         int s2 = Integer.parseInt(br.readLine());
+         int sumacion=s1+s2;
+         System.out.println ("La suma es " + s1 + "+" + s2 +"="+ sumacion);
+      }
+      catch (IOException | NumberFormatException e)
+          
+      {
+      }
+    
+   }
+    
+    
+    
+    
+}
